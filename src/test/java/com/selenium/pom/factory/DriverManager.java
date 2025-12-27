d8a80dc378886b26bd910d1db7341fad26cd8923
aydinserbestnl <aydinserbestnl@gmail.com>
2025-12-27 15:34:07 +0100

    Add headless browser support via system property

--- src/test/java/com/selenium/pom/factory/DriverManager.java
+++ src/test/java/com/selenium/pom/factory/DriverManager.java
@@ -5,26 +5,53 @@ import com.selenium.pom.utils.ConfigLoader;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.time.Duration;
 
 
 
 public class DriverManager {
+
     public WebDriver initializeDriver() {
         return initializeDriver(ConfigLoader.getInstance().getBrowser());
     }
 
     public WebDriver initializeDriver(String browser) {
+
+        boolean headless = Boolean.parseBoolean(
+                System.getProperty("headless", "false")
+        );
+
         WebDriver driver = switch (BrowserType.valueOf(browser.toUpperCase())) {
-            case CHROME -> new ChromeDriver();
-            case FIREFOX -> new FirefoxDriver();
+
+            case CHROME -> {
+                ChromeOptions options = new ChromeOptions();
+                if (headless) {
+                    options.addArguments("--headless=new");
+                    options.addArguments("--window-size=1920,1080");
+                }
+                yield new ChromeDriver(options);
+            }
+
+            case FIREFOX -> {
+                FirefoxOptions options = new FirefoxOptions();
+                if (headless) {
+                    options.addArguments("-headless");
+                }
+                yield new FirefoxDriver(options);
+            }
         };
 
-        driver.manage().window().maximize();
+        if (!headless) {
+            driver.manage().window().maximize();
+        }
+
         int implicit = ConfigLoader.getInstance().getImplicitWaitSeconds();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicit));
+
         return driver;
     }
 }
